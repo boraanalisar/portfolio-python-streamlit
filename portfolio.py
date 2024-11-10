7bceb7da86b3113f8c375d1dc1662e5cccefda16
boraanalisar <121869140+boraanalisar@users.noreply.github.com>
2024-11-10 11:55:50 -0300

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import pandas as pd
+from converter_pdf_para_excel import converter_pdf_para_excel
 
 # Título do portfólio
 st.title("Portfólio de Scripts Python - José Carlos")
@@ -7,11 +7,26 @@ st.title("Portfólio de Scripts Python - José Carlos")
 # Menu lateral
 menu = st.sidebar.selectbox("Escolha a categoria:", ["Pdf para Excel"])
 
-# Categoria: Calculadoras
+# Categoria: PDF para Excel
 if menu == "Pdf para Excel":
     st.header("Converte TIPI em PDF para Excel")
+
+    # Upload do arquivo PDF
+    uploaded_file = st.file_uploader("Envie o arquivo TIPI em PDF", type="pdf")
     
-
-
-
-
+    if uploaded_file is not None:
+        try:
+            # Converter PDF para Excel
+            excel_data = converter_pdf_para_excel(uploaded_file)
+            
+            # Download do Excel
+            st.download_button(
+                label="Baixar arquivo Excel",
+                data=excel_data,
+                file_name="TIPI_Dados.xlsx",
+                mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            )
+            st.success("Arquivo processado e pronto para download!")
+        
+        except Exception as e:
+            st.error(f"Ocorreu um erro ao processar o arquivo: {e}")
